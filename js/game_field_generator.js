2eb486f8af7dda376e9172e49f86807a3c8861c4
Vitalii Svinchyak <vitalii.svinchyak@andersenlab.com>
2017-09-14 18:00:12 +0300

    try smarter map generation

--- js/game_field_generator.js
+++ js/game_field_generator.js
@@ -1,40 +1,101 @@
 class GameFieldGenerator {
 
     gameMap(rows, cells) {
-        const field = mapGenerator.empty(rows, cells);
+        this.size = {y: rows, x: cells};
+        this.field = mapGenerator.empty(rows, cells);
         let item;
 
         for (let y = 0; y <= rows; y++) {
-            field[y] = [];
-
             for (let x = 0; x <= cells; x++) {
                 if (y === 0 || y === rows || x === 0 || x === cells) {
                     item = type.wall;
                 } else {
-                    const rand = getRandomInt(0, 6);
+                    const rand = getRandomInt(0, 20);
                     switch (rand) {
                         case 3:
-                            item = type.wall;
+                            this.generateChain(y, x, type.wall);
                             break;
                         case 4:
-                            item = type.tree;
+                            this.generateChain(y, x, type.tree);
                             break;
                         default:
-                            item = type.empty;
+                            this.field[y][x] = type.empty;
                             break;
                     }
+                    // const rand = getRandomInt(0, 6);
+                    // switch (rand) {
+                    //     case 3:
+                    //         item = type.wall;
+                    //         break;
+                    //     case 4:
+                    //         item = type.tree;
+                    //         break;
+                    //     default:
+                    //         item = type.empty;
+                    //         break;
+                    // }
                 }
 
-                field[y][x] = item;
+                // this.field[y][x] = item;
             }
         }
 
-        field[1][1] = type.animal;
-        field[1][field[0].length - 2] = type.animal;
-        field[field.length - 2][field[0].length - 2] = type.animal;
-        field[field.length - 2][1] = type.animal;
+        this.field[1][1] = type.animal;
+        this.field[1][this.field[0].length - 2] = type.animal;
+        this.field[this.field.length - 2][this.field[0].length - 2] = type.animal;
+        this.field[this.field.length - 2][1] = type.animal;
+        console.log(this.field);
 
-        return field;
+        return this.field;
+    }
+
+    generateChain(y, x, type) {
+        let startY = y;
+        let startX = x;
+
+        for (let i = 0; i < 10; i++) {
+            const variant = this.getRandomVariant(startY, startX);
+            if (variant) {
+                startY = variant.y;
+                startX = variant.x;
+                this.field[startY][startX] = type;
+            }
+        }
+    }
+
+    getRandomVariant(y, x) {
+        const variants = this.getPossiblePointsToGo(y, x);
+        return variants[getRandomInt(0, variants.length - 1)]
+    }
+
+    getPossiblePointsToGo(startY, startX) {
+        const variants = [];
+
+        for (let y = startY - 1; y <= startY + 1; y++) {
+            for (let x = startX - 1; x <= startX + 1; x++) {
+                if (!(y === 0 || y === this.size.y || x === 0 || x === this.size.x)
+                    && solidObjects.indexOf(this.field[y][x]) === -1
+                    && this.nearIsEmpty(y, x)
+                ) {
+                    variants.push({x: x, y: y});
+                }
+            }
+        }
+
+        return variants;
+    }
+
+    nearIsEmpty(startY, startX) {
+        let count = 0;
+        for (let y = startY - 1; y <= startY + 1; y++) {
+            for (let x = startX - 1; x <= startX + 1; x++) {
+                if (solidObjects.indexOf(this.field[y][x]) !== -1) {
+                    count++;
+                }
+            }
+        }
+
+        return count <= 2;
     }
 }
 const gameFieldGenerator = new GameFieldGenerator();
